75d5b070f1a30cc7132e297de7f33356e0b9e233
Andrea <lollosositv@gmail.com>
2025-11-29 10:40:44 +0100

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.example.bruxismdetector"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.3.8"
+        versionCode = 6
+        versionName = "1.3.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
