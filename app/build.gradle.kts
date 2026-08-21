8261fb39ee958815d54705d1454da056f25b130a
Andrea <lollosositv@gmail.com>
2026-08-21 13:24:58 +0200

    Updated version name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 7
-        versionName = "1.3.9"
+        versionName = "1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
