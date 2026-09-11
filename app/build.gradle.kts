b6b5e1c7fc55a7625264c9fdb0731f122ae53b10
Andrea <lollosositv@gmail.com>
2026-09-11 19:30:39 +0200

    New version push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.bruxismdetector"
         minSdk = 26
         targetSdk = 37
-        versionCode = 14
-        versionName = "1.4.6"
+        versionCode = 15
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
